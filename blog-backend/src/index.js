a3441d6ef045c868cc0b33428ab62f17be31616c
YeonjuSeo <tjduswn1219@gmail.com>
2021-08-21 16:03:33 +0900

    🚀  21.5.1 koa-router 사용

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,29 +1,20 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 const app = new Koa();
-app.use((ctx, next) => {
-  console.log(ctx.url);
-  console.log(1);
-  if (ctx.query.authorized !== '1') {
-    ctx.status = 401; //Unauthorized
-    return;
-  }
-  next().then(() => {
-    console.log('END');
-  });
+const router = new Router();
+
+// 라우터 설정
+router.get('/', (ctx) => {
+  ctx.body = '홈';
 });
-app.use((ctx, next) => {
-  console.log(2);
-  next();
+
+router.get('/about', (ctx) => {
+  ctx.body = '소개';
 });
-app.use((ctx) => {
-  ctx.body = 'hello world';
-});
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
+
 app.listen(4000, () => {
   console.log('Listening to port 4000');
 });
-
-// 콘솔 출력 결과
-// /?authhorized=1
-// 1
-// 2
-// END
